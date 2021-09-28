bccae4f7312ef918a88c53ec2c71ce969df01f38
ioananicolae8 <ioananicolae12312@gmail.com>
2021-09-28 16:01:37 +0300

    commit graphQL

--- src/features/helloWorld/HelloWorld.js
+++ src/features/helloWorld/HelloWorld.js
@@ -1,7 +1,16 @@
+import { useQuery } from "@apollo/client";
+import React from 'react'
+import { MY_FIRST_QUERY } from "./queries/MyFirstQuery";
+import { LoadingFakeText } from '@bit/totalsoft_oss.react-mui.kit.core';
 
 function HelloWorld() {
 
-    return "Hello World!🐱‍💻"
+    const { loading, data } = useQuery(MY_FIRST_QUERY);
+    if (loading) {
+        return <LoadingFakeText lines={10} />
+    }
+
+    return data?.myFirstEndpoint
 }
 
 export default HelloWorld;
